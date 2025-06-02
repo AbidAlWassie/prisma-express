25a879f6078b7130cf17d84297f855181cb4472b
Abid Al Wassie <abidalwassie@gmail.com>
2025-06-02 07:39:18 +0600

    fix: remove file extensions from route imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import postRoutes from "./routes/post.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
